d8f68e62198b51a34a3914ba44fd1f17d4676377
Ilydanyxx <danya.chuyko@icloud.com>
2025-05-06 00:25:47 +0300

    c

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import products from '../api/products';
+
 
 const Product = ({ addToCart }) => {
   const { id } = useParams();
